95a7f307b1e4063665abda412d1ae82b199b093c
Kupono Kealiinohomoku <kuponokealiinohomoku@AthinaspleWatch.localdomain>
2020-11-05 23:32:39 -0800

    Completed all challenges

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ def sort_letters(message):
     and return the new string."""
     return ''.join(sorted(list(message)))
 
-
 @app.route('/')
 def homepage():
     """A homepage with handy links for your convenience."""
@@ -17,57 +16,70 @@ def homepage():
 @app.route('/froyo')
 def choose_froyo():
     """Shows a form to collect the user's Fro-Yo order."""
-    pass
+    return render_template('froyo_form.html')
 
 @app.route('/froyo_results')
 def show_froyo_results():
     """Shows the user what they ordered from the previous page."""
-    pass
+    users_froyo_flavor = request.args.get('flavor')
+    users_topping = request.args.get('topping')
+    return f'You ordered {users_froyo_flavor} flavored Fro-Yo and your toppings are {users_topping}!'
 
 @app.route('/favorites')
 def favorites():
     """Shows the user a form to choose their favorite color, animal, and city."""
-    pass
+    return """
+    <form action="/favorites_results" method="GET">
+    What is your all-time favorite color, animal, and city? <br/>
+        <input type="text" name="color"> <br/>
+        <input type="text" name="animal"> <br/>
+        <input type="text" name="city"> <br/>
+        <input type="submit" value="submit">
+    </form>
+    """
 
 @app.route('/favorites_results')
 def favorites_results():
     """Shows the user a nice message using their form results."""
-    pass
+    users_color = request.args.get("color")
+    users_animal = request.args.get("animal")
+    users_city = request.args.get("city")
+    return f'Wow! I did not know {users_color} {users_animal} lived in {users_city}!'
 
 @app.route('/secret_message')
 def secret_message():
     """Shows the user a form to collect a secret message. Sends the result via
     the POST method to keep it a secret!"""
-    pass
+    return """
+    <form action='/message_results' method="POST">
+        Enter a Secret Message: <br/>
+        <input type="text" name="message"> <br/>
+        <input type="submit" value="submit">
+    </form>
+    """
 
 @app.route('/message_results', methods=['POST'])
 def message_results():
     """Shows the user their message, with the letters in sorted order."""
-    pass
+    secret = request.form.get('message')
+    arranged_letters = sort_letters(secret)
+    return f'Here is your secret message: {arranged_letters}'
 
 @app.route('/calculator')
 def calculator():
     """Shows the user a form to enter 2 numbers and an operation."""
-    return """
-    <form action="/calculator_results" method="GET">
-        Please enter 2 numbers and select an operator.<br/><br/>
-        <input type="number" name="operand1">
-        <select name="operation">
-            <option value="add">+</option>
-            <option value="subtract">-</option>
-            <option value="multiply">*</option>
-            <option value="divide">/</option>
-        </select>
-        <input type="number" name="operand2">
-        <input type="submit" value="Submit!">
-    </form>
-    """
+    return render_template('calculator_form.html')
 
-@app.route('/calculator_results')
+    @app.route('/calculator_results')
 def calculator_results():
     """Shows the user the result of their calculation."""
-    pass
+    context  = {
+        'number1': int(request.args.get('operand1')),
+        'number2': int(request.args.get('operand2')),
+        'operator': str(request.args.get('operation'))
+    }
 
+    return render_template('calculator_results.html', **context)
 
 # List of compliments to be used in the `compliments_results` route (feel free 
 # to add your own!) 
@@ -109,9 +121,6 @@ def compliments_results():
     context = {
         # TODO: Enter your context variables here.
     }
-
-    return render_template('compliments_results.html', **context)
-
 
 if __name__ == '__main__':
     app.run()
